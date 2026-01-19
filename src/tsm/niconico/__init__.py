7be18286d106b3e72b23301d51896c8ac3de7a19
gcrtnst <52910071+gcrtnst@users.noreply.github.com>
2026-01-19 18:08:24 +0900

    Explicitly import exceptions in niconico module

--- src/tsm/niconico/__init__.py
+++ src/tsm/niconico/__init__.py
@@ -1,2 +1,16 @@
 from .client import Niconico  # noqa: F401
-from .exceptions import *
+from .exceptions import (
+    CommunicationError,  # noqa: F401
+    ContentSearchError,  # noqa: F401
+    InvalidContentID,  # noqa: F401
+    InvalidResponse,  # noqa: F401
+    LoginFailed,  # noqa: F401
+    LoginRequired,  # noqa: F401
+    NiconicoException,  # noqa: F401
+    NotFound,  # noqa: F401
+    TSAlreadyRegistered,  # noqa: F401
+    TSMaxReservation,  # noqa: F401
+    TSNotSupported,  # noqa: F401
+    TSRegistrationExpired,  # noqa: F401
+    Timeout,  # noqa: F401
+)

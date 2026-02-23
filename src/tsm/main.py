93b702942ba177cebc3190bd9dbdf764a53fa277
gcrtnst <52910071+gcrtnst@users.noreply.github.com>
2026-02-23 21:08:31 +0900

    Use fully qualified names for imports in main.py

--- src/tsm/main.py
+++ src/tsm/main.py
@@ -1,22 +1,21 @@
+import argparse
 import contextlib
+import http.cookiejar
 import json
+import pathlib
 import sys
-from argparse import ArgumentParser
-from http.cookiejar import LWPCookieJar
-from json import JSONDecodeError
-from pathlib import Path
-from tomllib import TOMLDecodeError
+import tomllib
 
-from . import config as tsm_config
-from .tsm import TSMachine, Filter
+import tsm
+import tsm.config
 
 
 @contextlib.contextmanager
 def lwp_cookiejar(filename=None, filemode=0o666):
     if filename is not None:
-        filename = Path(filename)
+        filename = pathlib.Path(filename)
 
-    jar = LWPCookieJar()
+    jar = http.cookiejar.LWPCookieJar()
     if filename is not None and filename.exists():
         jar.load(str(filename))
     try:
@@ -28,12 +27,12 @@ def lwp_cookiejar(filename=None, filemode=0o666):
 
 
 def main():
-    argp = ArgumentParser()
+    argp = argparse.ArgumentParser()
     argp.add_argument(
         "-c",
         "--config",
-        type=Path,
-        default=Path("~", ".config", "tsm", "config.toml").expanduser(),
+        type=pathlib.Path,
+        default=pathlib.Path("~", ".config", "tsm", "config.toml").expanduser(),
         help="TOML-formatted configuration file (default: %(default)s)",
     )
     argp.add_argument(
@@ -52,12 +51,12 @@ def main():
     argv = argp.parse_args()
 
     try:
-        config = tsm_config.load(argv.config)
+        config = tsm.config.load(argv.config)
     except OSError as e:
         sys.exit("error: config '{}': {}".format(argv.config, e.strerror))
-    except TOMLDecodeError as e:
+    except tomllib.TOMLDecodeError as e:
         sys.exit("error: config: toml: {}".format(e))
-    except tsm_config.ConfigError as e:
+    except tsm.config.ConfigError as e:
         sys.exit("error: config: {}".format(e))
 
     filter_list = []
@@ -73,11 +72,11 @@ def main():
                         filter_cfg.jsonFilter, e.strerror
                     )
                 )
-            except JSONDecodeError as e:
+            except json.JSONDecodeError as e:
                 sys.exit("error: jsonFilter: {}".format(e))
 
         filter_list.append(
-            Filter(
+            tsm.Filter(
                 q=filter_cfg.q,
                 targets=filter_cfg.targets,
                 sort=filter_cfg.sort,
@@ -94,26 +93,26 @@ def main():
 
     with (
         lwp_cookiejar(filename=config.login.cookieJar, filemode=0o600) as jar,
-        TSMachine() as tsm,
+        tsm.TSMachine() as mac,
     ):
-        tsm.mail = config.login.mail
-        tsm.password = config.login.password
-        tsm.cookies = jar
-        tsm.timeout = config.misc.timeout
-        tsm.user_agent = config.misc.userAgent
-        tsm.context = config.misc.context
-        tsm.filter_list = filter_list
-        tsm.overwrite = config.misc.overwrite
-        tsm.warnings = set()
+        mac.mail = config.login.mail
+        mac.password = config.login.password
+        mac.cookies = jar
+        mac.timeout = config.misc.timeout
+        mac.user_agent = config.misc.userAgent
+        mac.context = config.misc.context
+        mac.filter_list = filter_list
+        mac.overwrite = config.misc.overwrite
+        mac.warnings = set()
         if config.warn.tsNotSupported:
-            tsm.warnings.add("ts_not_supported")
+            mac.warnings.add("ts_not_supported")
         if config.warn.tsRegistrationExpired:
-            tsm.warnings.add("ts_registration_expired")
+            mac.warnings.add("ts_registration_expired")
         if config.warn.tsMaxReservation:
-            tsm.warnings.add("ts_max_reservation")
+            mac.warnings.add("ts_max_reservation")
         if argv.search is not None:
-            sys.exit(tsm.run_search_only(argv.search))
-        sys.exit(tsm.run_auto_reserve())
+            sys.exit(mac.run_search_only(argv.search))
+        sys.exit(mac.run_auto_reserve())
 
 
 if __name__ == "__main__":

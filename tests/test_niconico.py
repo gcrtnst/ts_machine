b99619eb9c55c7e85fdd05f1e757ddb568df3308
gcrtnst <52910071+gcrtnst@users.noreply.github.com>
2026-02-23 20:35:32 +0900

    Use fully qualified names for imports in test_niconico.py

--- tests/test_niconico.py
+++ tests/test_niconico.py
@@ -1,102 +1,118 @@
 import unittest
-from unittest.mock import Mock, call
+import unittest.mock
 
 import tsm.niconico.client
 import tsm.niconico.utils
-from tsm.niconico.client import Niconico
-from tsm.niconico.exceptions import InvalidContentID, LoginFailed, LoginRequired
+import tsm.niconico.exceptions
 
 
 class TestNiconico(unittest.TestCase):
     def test_login_if_required(self):
-        n = Mock(spec_set=Niconico())
+        n = unittest.mock.Mock(spec_set=tsm.niconico.client.Niconico())
         n.mail = None
         n.password = None
-        func = Mock()
+        func = unittest.mock.Mock()
         tsm.niconico.client._login_if_required(func)(n)
         self.assertEqual(n.mock_calls, [])
-        self.assertEqual(func.mock_calls, [call(n)])
+        self.assertEqual(func.mock_calls, [unittest.mock.call(n)])
 
-        n = Mock(spec_set=Niconico())
+        n = unittest.mock.Mock(spec_set=tsm.niconico.client.Niconico())
         n.mail = None
         n.password = None
-        func = Mock(side_effect=LoginRequired)
-        with self.assertRaises(LoginRequired):
+        func = unittest.mock.Mock(side_effect=tsm.niconico.exceptions.LoginRequired)
+        with self.assertRaises(tsm.niconico.exceptions.LoginRequired):
             tsm.niconico.client._login_if_required(func)(n)
         self.assertEqual(n.mock_calls, [])
-        self.assertEqual(func.mock_calls, [call(n)])
+        self.assertEqual(func.mock_calls, [unittest.mock.call(n)])
 
-        n = Mock(spec_set=Niconico())
+        n = unittest.mock.Mock(spec_set=tsm.niconico.client.Niconico())
         n.mail = "email@example.com"
         n.password = "password"
-        func = Mock(side_effect=[LoginRequired, None])
+        func = unittest.mock.Mock(
+            side_effect=[tsm.niconico.exceptions.LoginRequired, None]
+        )
         tsm.niconico.client._login_if_required(func)(n)
-        self.assertEqual(n.mock_calls, [call.login()])
-        self.assertEqual(func.mock_calls, [call(n), call(n)])
+        self.assertEqual(n.mock_calls, [unittest.mock.call.login()])
+        self.assertEqual(
+            func.mock_calls, [unittest.mock.call(n), unittest.mock.call(n)]
+        )
 
-        n = Mock(spec_set=Niconico())
+        n = unittest.mock.Mock(spec_set=tsm.niconico.client.Niconico())
         n.mail = "email@example.com"
         n.password = "password"
-        func = Mock(side_effect=[LoginRequired, LoginRequired])
-        with self.assertRaises(LoginFailed):
+        func = unittest.mock.Mock(
+            side_effect=[
+                tsm.niconico.exceptions.LoginRequired,
+                tsm.niconico.exceptions.LoginRequired,
+            ]
+        )
+        with self.assertRaises(tsm.niconico.exceptions.LoginFailed):
             tsm.niconico.client._login_if_required(func)(n)
-        self.assertEqual(n.mock_calls, [call.login()])
-        self.assertEqual(func.mock_calls, [call(n), call(n)])
+        self.assertEqual(n.mock_calls, [unittest.mock.call.login()])
+        self.assertEqual(
+            func.mock_calls, [unittest.mock.call(n), unittest.mock.call(n)]
+        )
 
     def test_http_request(self):
-        n = Niconico()
+        n = tsm.niconico.client.Niconico()
         n.timeout = None
-        n._session = Mock(spec_set=n._session)
+        n._session = unittest.mock.Mock(spec_set=n._session)
         n._http_request("method", "url")
         self.assertEqual(
-            n._session.mock_calls, [call.request("method", "url", timeout=None)]
+            n._session.mock_calls,
+            [unittest.mock.call.request("method", "url", timeout=None)],
         )
 
-        n = Niconico()
+        n = tsm.niconico.client.Niconico()
         n.timeout = 1
-        n._session = Mock(spec_set=n._session)
+        n._session = unittest.mock.Mock(spec_set=n._session)
         n._http_request("method", "url")
         self.assertEqual(
-            n._session.mock_calls, [call.request("method", "url", timeout=1)]
+            n._session.mock_calls,
+            [unittest.mock.call.request("method", "url", timeout=1)],
         )
 
-        n = Niconico()
+        n = tsm.niconico.client.Niconico()
         n.timeout = None
-        n._session = Mock(spec_set=n._session)
+        n._session = unittest.mock.Mock(spec_set=n._session)
         n._http_request("method", "url", timeout=2)
         self.assertEqual(
-            n._session.mock_calls, [call.request("method", "url", timeout=2)]
+            n._session.mock_calls,
+            [unittest.mock.call.request("method", "url", timeout=2)],
         )
 
-        n = Niconico()
+        n = tsm.niconico.client.Niconico()
         n.timeout = 1
-        n._session = Mock(spec_set=n._session)
+        n._session = unittest.mock.Mock(spec_set=n._session)
         n._http_request("method", "url", timeout=2)
         self.assertEqual(
-            n._session.mock_calls, [call.request("method", "url", timeout=2)]
+            n._session.mock_calls,
+            [unittest.mock.call.request("method", "url", timeout=2)],
         )
 
     def test_login(self):
-        n = Niconico()
+        n = tsm.niconico.client.Niconico()
         n.mail = None
         n.password = None
-        n._http_request = Mock(spec_set=n._http_request)
-        with self.assertRaises(LoginFailed):
+        n._http_request = unittest.mock.Mock(spec_set=n._http_request)
+        with self.assertRaises(tsm.niconico.exceptions.LoginFailed):
             n.login()
         self.assertEqual(n._http_request.mock_calls, [])
 
-        resp = Mock()
+        resp = unittest.mock.Mock()
         resp.cookies = []
-        n = Niconico()
+        n = tsm.niconico.client.Niconico()
         n.mail = "mail@example.com"
         n.password = "password"
-        n._http_request = Mock(spec_set=n._http_request, return_value=resp)
-        with self.assertRaises(LoginFailed):
+        n._http_request = unittest.mock.Mock(
+            spec_set=n._http_request, return_value=resp
+        )
+        with self.assertRaises(tsm.niconico.exceptions.LoginFailed):
             n.login()
         self.assertEqual(
             n._http_request.mock_calls,
             [
-                call(
+                unittest.mock.call(
                     "post",
                     "https://account.nicovideo.jp/api/v1/login",
                     data={
@@ -107,21 +123,23 @@ class TestNiconico(unittest.TestCase):
                 )
             ],
         )
-        self.assertEqual(resp.mock_calls, [call.raise_for_status()])
+        self.assertEqual(resp.mock_calls, [unittest.mock.call.raise_for_status()])
 
-        cookie = Mock()
+        cookie = unittest.mock.Mock()
         cookie.name = "user_session"
-        resp = Mock()
+        resp = unittest.mock.Mock()
         resp.cookies = [cookie]
-        n = Niconico()
+        n = tsm.niconico.client.Niconico()
         n.mail = "mail@example.com"
         n.password = "password"
-        n._http_request = Mock(spec_set=n._http_request, return_value=resp)
+        n._http_request = unittest.mock.Mock(
+            spec_set=n._http_request, return_value=resp
+        )
         n.login()
         self.assertEqual(
             n._http_request.mock_calls,
             [
-                call(
+                unittest.mock.call(
                     "post",
                     "https://account.nicovideo.jp/api/v1/login",
                     data={
@@ -132,7 +150,7 @@ class TestNiconico(unittest.TestCase):
                 )
             ],
         )
-        self.assertEqual(resp.mock_calls, [call.raise_for_status()])
+        self.assertEqual(resp.mock_calls, [unittest.mock.call.raise_for_status()])
 
 
 class TestNiconicoUtils(unittest.TestCase):
@@ -140,5 +158,5 @@ class TestNiconicoUtils(unittest.TestCase):
         for c in [(10, (None, 10)), ("10", (None, 10)), ("lv10", ("lv", 10))]:
             self.assertEqual(tsm.niconico.utils.parse_id(c[0]), c[1])
 
-        with self.assertRaises(InvalidContentID):
+        with self.assertRaises(tsm.niconico.exceptions.InvalidContentID):
             tsm.niconico.utils.parse_id("lv")
